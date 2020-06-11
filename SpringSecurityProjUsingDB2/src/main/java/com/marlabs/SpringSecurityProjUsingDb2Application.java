dcd4ef03e189fcf96ea703eaff632613e9025c98
Rav52ant <Rav52ant@DESKTOP-GUGU8QH>
2020-06-11 11:02:42 +0530

    test

--- SpringSecurityProjUsingDB2/src/main/java/com/marlabs/SpringSecurityProjUsingDb2Application.java
+++ SpringSecurityProjUsingDB2/src/main/java/com/marlabs/SpringSecurityProjUsingDb2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityProjUsingDb2Application {
 
 	public static void main(String[] args) {
+		//
 		SpringApplication.run(SpringSecurityProjUsingDb2Application.class, args);
 	}
 
